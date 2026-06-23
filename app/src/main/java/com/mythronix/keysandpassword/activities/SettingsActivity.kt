5e8c3370f47773f0199011312469243e2d4902a6
MythroniX24 <kavitagupta2405@gmail.com>
2026-06-23 19:11:40 +0530

    fix missing view port in settingsActivity

--- app/src/main/java/com/mythronix/keysandpassword/activities/SettingsActivity.kt
+++ app/src/main/java/com/mythronix/keysandpassword/activities/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.view.MenuItem
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
